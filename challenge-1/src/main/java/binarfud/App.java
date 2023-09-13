01d255a113b6a484ec9817b0a3e235aa6d4756b2
aljovanmp1 <141197702+aljovanmp1@users.noreply.github.com>
2023-09-13 11:09:10 +0700

    add write to txt file functionalities

--- challenge-1/src/main/java/binarfud/App.java
+++ challenge-1/src/main/java/binarfud/App.java
@@ -3,7 +3,7 @@ package binarfud;
 public class App 
 {
     public static void main( String[] args )
-    {
+    {   
         PlaceOrder newOrder = new PlaceOrder();
 
         newOrder.receivedOrder();
--- challenge-1/src/main/java/binarfud/PlaceOrder.java
+++ challenge-1/src/main/java/binarfud/PlaceOrder.java
@@ -1,6 +1,11 @@
 package binarfud;
+
 import java.util.LinkedHashMap;
 import java.util.Scanner;
+
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.io.IOException;
 
 public class PlaceOrder {
     boolean orderIsFinished = false;
@@ -10,71 +15,82 @@ public class PlaceOrder {
     Scanner input = new Scanner(System.in);
     String state;
     String selectedMenu;
-    
-    PlaceOrder(){
+    String savePath = "./struk.txt";
+
+    PlaceOrder() {
         this.state = "menu";
-        this.menu = new LinkedHashMap<String, Integer>(5, 0.75f){{
-            put("Nasi Goreng", 15000);
-            put("Mie Goreng", 13000);
-            put("Nasi + Ayam", 18000);
-            put("Es Teh Manis", 3000);
-            put("Es Jeruk", 5000);
-        }};
+        this.menu = new LinkedHashMap<String, Integer>(5, 0.75f) {
+            {
+                put("Nasi Goreng", 15000);
+                put("Mie Goreng", 13000);
+                put("Nasi + Ayam", 18000);
+                put("Es Teh Manis", 3000);
+                put("Es Jeruk", 5000);
+            }
+        };
 
         this.order = new LinkedHashMap<String, Integer>();
     }
 
-    void receivedOrder(){
-        while(!this.orderIsFinished){
-            switch (this.state) {
-                case "menu":
-                    printMenu();
-                    break;
-                case "menuSelected":
-                    printSelectedMenu();
-                    break;
-                case "confirmation":
-                    printConfirmation();
-                    break;
-                case "invoice":
-                    printInvoice();
-                    break;
-                case "exit":
-                    this.orderIsFinished = true;
-                    break;
+    void receivedOrder() {
+        try {
+            while (!this.orderIsFinished) {
+                switch (this.state) {
+                    case "menu":
+                        printMenu();
+                        break;
+                    case "menuSelected":
+                        printSelectedMenu();
+                        break;
+                    case "confirmation":
+                        printConfirmation();
+                        break;
+                    case "invoice":
+                        // printInvoice();
+                        System.out.print(getInvoice());
+                        saveToFile();
+                        break;
+                    case "exit":
+                        this.orderIsFinished = true;
+                        break;
+                }
             }
+
+        } catch (IOException e) {
+            System.out.println("Something went wrong: " + e.getMessage());
         }
-    }    
-    
-    void printMenu(){
+    }
+
+    void printMenu() {
         printHeader();
         int ind = 1;
         System.out.println("Silahkan pilih makanan : ");
         for (String key : menu.keySet()) {
-            System.out.printf("%d. %-13s| %d.000%n", ind, key, menu.get(key)/1000);
+            System.out.printf("%d. %-13s| %d.000%n", ind, key, menu.get(key) / 1000);
             ind++;
         }
         System.out.println("99. Pesan dan Bayar");
         System.out.println("0. Keluar Aplikasi");
         System.out.println();
-        
 
         int number = -1;
-        
+
         boolean dataInputIsCorrect = false;
-        while(!dataInputIsCorrect){
+        while (!dataInputIsCorrect) {
             System.out.print("=> ");
             number = handleIntInput();
 
-            if (number == -1) continue;
+            if (number == -1)
+                continue;
 
-            else if (number>menu.keySet().toArray().length && number != 99)
+            else if (number > menu.keySet().toArray().length && number != 99)
                 System.out.println("Anda hanya dapat memilih menu yang tersedia");
-            
-            else dataInputIsCorrect=true;
+
+            else
+                dataInputIsCorrect = true;
         }
 
-        switch (number){
+        switch (number) {
             case 0:
                 this.state = "exit";
                 return;
@@ -84,39 +100,41 @@ public class PlaceOrder {
         }
 
         this.state = "menuSelected";
-        this.selectedMenu = menu.keySet().toArray()[number-1].toString();
+        this.selectedMenu = menu.keySet().toArray()[number - 1].toString();
     }
 
-    void printSelectedMenu(){
+    void printSelectedMenu() {
         printHeader();
-        System.out.printf("%-13s| %d.000%n", this.selectedMenu, menu.get(this.selectedMenu)/1000);
+        System.out.printf("%-13s| %d.000%n", this.selectedMenu, menu.get(this.selectedMenu) / 1000);
         System.out.println("(input 0 untuk kembali)");
         System.out.println();
 
         int number = -1;
         boolean dataInputIsCorrect = false;
 
-        while(!dataInputIsCorrect){
+        while (!dataInputIsCorrect) {
             System.out.print("qty => ");
             number = handleIntInput();
 
-            if (number == -1) continue;
-            else dataInputIsCorrect=true;
+            if (number == -1)
+                continue;
+            else
+                dataInputIsCorrect = true;
         }
 
         if (number == 0) {
-            this.state = "menu";     
+            this.state = "menu";
             return;
         }
-        
+
         this.order.put(this.selectedMenu, number);
         this.state = "menu";
     }
 
-    void printConfirmation(){
+    void printConfirmation() {
         printHeader();
-        printOrder();
-        
+        System.out.print(getOrder());
+
         System.out.println();
         System.out.println("1. Konfirmasi dan Bayar");
         System.out.println("2. Kembali ke menu utama");
@@ -125,15 +143,17 @@ public class PlaceOrder {
 
         int number = -1;
         boolean dataInputIsCorrect = false;
-        while(!dataInputIsCorrect){
+        while (!dataInputIsCorrect) {
             System.out.print("=> ");
             number = handleIntInput();
 
-            if (number == -1) continue;            
-            else dataInputIsCorrect=true;
+            if (number == -1)
+                continue;
+            else
+                dataInputIsCorrect = true;
         }
 
-        switch (number){
+        switch (number) {
             case 1:
                 if (order.keySet().toArray().length < 1) {
                     System.out.println("Silahkan masukkan pesanan terlebih dahulu");
@@ -152,86 +172,91 @@ public class PlaceOrder {
 
     }
 
-    void printInvoice(){
-        printHeader();
+    String getInvoice() {
+        String result = "";
+        result += determineHeaderContent("BinarFud");
 
-        System.out.println("Terima kasih sudah memesan");
-        System.out.println("di BinarFud");
-        System.out.println();
+        result += "Terima kasih sudah memesan\n";
+        result += "di BinarFud\n\n";
 
-        System.out.println("Dibawah ini adalah pesanan anda");
-        System.out.println();
-        printOrder();
-        System.out.println();
-        System.out.println("Pembayaran : BinarCash");
+        result += "Dibawah ini adalah pesanan anda\n";
 
-        System.out.println();
-        System.out.println("========================");
-        System.out.println("Simpan struk ini sebagai");
-        System.out.println("bukti pembayaran");
-        System.out.println("========================");
-        System.out.println();
-        
+        result += getOrder();
+        result += "\nPembayaran : BinarCash\n";
+
+        result += "\n========================\n";
+        result += "Simpan struk ini sebagai\n";
+        result += "bukti pembayaran\n";
+        result += "========================\n\n";
+
         this.state = "exit";
+        return result;
+
     }
 
-    void printOrder(){
+    void saveToFile() throws IOException {
+        boolean isAppend = false;
+        FileWriter writer = new FileWriter(this.savePath, isAppend);
+        PrintWriter printLine = new PrintWriter(writer);
+
+        printLine.print(getInvoice());
+        printLine.close();
+    }
+
+    String getOrder() {
+        String result = "";
         int totalItem = 0;
         int totalPrice = 0;
 
-        if (order.keySet().toArray().length < 1) {
-            System.out.println("----------------------------+");
-            System.out.printf("%-14s %-5s %s.000 %n", "Total", totalItem, (totalPrice)/1000);
+        for (String key : order.keySet()) {
+            result += String.format("%-14s %-5s %s.000 %n", key, order.get(key),
+                    (order.get(key) * menu.get(key)) / 1000);
+            totalItem += order.get(key);
+            totalPrice += order.get(key) * menu.get(key);
         }
 
-        else {
-            for (String key : menu.keySet()) {
-                System.out.printf("%-14s %-5s %s.000 %n", key, order.get(key), (order.get(key)*menu.get(key))/1000);
-                totalItem+=order.get(key);
-                totalPrice+=order.get(key)*menu.get(key);
-            }
-    
-            System.out.println("----------------------------+");
-            System.out.printf("%-14s %-5s %s.000 %n", "Total", totalItem, (totalPrice)/1000);
+        result += "----------------------------+\n";
+        result += String.format("%-14s %-5s %s.000 %n", "Total", totalItem, (totalPrice) / 1000);
 
-        }
+        return result;
     }
 
-    void printHeader(){
-        switch(this.state) {
-            case "menu": 
-                determineHeaderContent("Selamat datang di BinarFud");   
+    void printHeader() {
+        switch (this.state) {
+            case "menu":
+                System.out.print(determineHeaderContent("Selamat datang di BinarFud"));
                 break;
-            case "menuSelected": 
-                determineHeaderContent("Berapa pesanan anda");
+            case "menuSelected":
+                System.out.print(determineHeaderContent("Berapa pesanan anda"));
                 break;
             case "confirmation":
-                determineHeaderContent("Konfirmasi & Pembayaran");
+                System.out.print(determineHeaderContent("Konfirmasi & Pembayaran"));
                 break;
             case "invoice":
-                determineHeaderContent("BinarFud");
+                System.out.print(determineHeaderContent("BinarFud"));
                 break;
-        }        
+        }
     }
 
-    void determineHeaderContent(String sentence) {
-        System.out.println();
-        System.out.println("========================");
-        System.out.println(sentence);
-        System.out.println("========================");
-        System.out.println();
+    String determineHeaderContent(String sentence) {
+        String result = "";
+        result += '\n';
+        result += "========================\n";
+        result += sentence + '\n';
+        result += "========================\n\n";
+        return result;
     }
 
-    int handleIntInput(){
-        try{
+    int handleIntInput() {
+        try {
             int num = Integer.parseInt(input.nextLine());
 
-            if (num <0) {
+            if (num < 0) {
                 System.out.println("Tidak bisa memasukkan nilai minus");
                 return -1;
             }
             return num;
-        } catch (Exception e){
+        } catch (Exception e) {
             System.out.println("Sepertinya anda tidak memasukkan angka");
             return -1;
         }
